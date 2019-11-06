277dfd0011623130eb2729d19b8f7caf6522e73e
Student <example@example.com>
2019-11-06 15:20:15 -0800

    update functionality of dice game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 // Business Logic
 
-function player(subTotal, total, roll) {
+function player(subTotal, total) {
   this.subTotal = subTotal; //points
   this.total = total; //bank
-  this.roll = 0;
 }
 
 player.prototype.addSubTotal = function() {
@@ -19,11 +18,12 @@ player.prototype.clearTotal = function() {
 }
 
 
-var player1 = new player(0, 0, 0);
-var player2 = new player(0, 0, 0);
+var player1 = new player(0, 0);
+var player2 = new player(0, 0);
 
 var rolls =[];
-
+var score = 0;
+var bank = 0;
  var rollDice = function() {
 
 
@@ -31,9 +31,10 @@ var rolls =[];
 
   rolls.push(roll);
 
-    //if player has points =+ roll total; player.addTotal();
     //if roll 1 then points clear and next player turn
 };
+
+
 
 // rolls.reduce(totalScore);
 
@@ -47,42 +48,49 @@ $(document).ready(function() {
 
 
   $(".btn-dark").click(function() {
-
     $(".game").show();
-
     $(".btn-dark").hide();
-
   });
-
   $(".rollButton").click(function () {
     rollDice();
-
     score = rolls.reduce(totalScore);
-
     function totalScore(total, num) {
       return total + num;
     }
 
     console.log(score);
 
-    if (roll === 1) {
-      roll = 0;
+    if (roll > 1) {
+      $(".currentRoll").text(roll);
+      $(".activePlayer .currentScore").text(score);
+
+     } else if (roll === 1) {
+      roll = "Oops! You rolled a 1!";
       score = 0;
+      rolls = [];
+      $(".currentRoll").text(roll);
+      $(".activePlayer .currentScore").text(score);
       $(".player1").toggleClass("activePlayer");
       $(".player2").toggleClass("activePlayer");
       // $(".currentRoll").toggle(".active");
-    }
-
-    $(".active").text(roll);
-    $(".currentScore").text(score);
-  });
+      }
+    });
 
 
-  $("form.rollTurn").submit(function(ev) {
-    ev.preventDefault();
+    $(".holdButton").click(function() {
 
+      if(score > 0) {
+        var bank = $(".activePlayer .bank").val();
+        bank += score;
+        score = 0;
+        rolls = [];
+        $(".currentRoll").text(roll);
+        $(".activePlayer .currentScore").text(score);
+      }
 
-
+      $(".activePlayer .bank").text(bank);
+      $(".player1").toggleClass("activePlayer");
+      $(".player2").toggleClass("activePlayer");
   });
 
 });

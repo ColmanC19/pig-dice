c8e93ae8b5d60fd589980e41b4255e31b4892eae
Student <example@example.com>
2019-11-06 11:38:10 -0800

    update styling, functionality of dice game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,88 @@
-function playerA(subTotal, total) {
-  this.subTotal = subTotal;
-  this.total = total;
+// Business Logic
+
+function player(subTotal, total, roll) {
+  this.subTotal = subTotal; //points
+  this.total = total; //bank
   this.roll = 0;
 }
 
-playerA.prototype.addTotal = function()
+player.prototype.addSubTotal = function() {
+  this.roll += subTotal;
+}
+
+player.prototype.addTotal = function() {
+   total += this.roll + subTotal;
+}
+
+player.prototype.clearTotal = function() {
+  this.subTotal = 0;
+}
+
+
+var player1 = new player(0, 0, 0);
+var player2 = new player(0, 0, 0);
+
+var rolls =[];
+
+ var rollDice = function() {
+
+
+  roll = Math.floor((Math.random() * 6) + 1);
+
+  rolls.push(roll);
+
+    //if player has points =+ roll total; player.addTotal();
+    //if roll 1 then points clear and next player turn
+};
+
+// rolls.reduce(totalScore);
+
+
+//User Logic
 
 
 
-// rollDice = Math.floor(Math.random() * 6) + 1;
+
+$(document).ready(function() {
+
+
+  $(".btn-dark").click(function() {
+
+    $(".game").show();
+
+    $(".btn-dark").hide();
+
+  });
+
+  $(".rollButton").click(function () {
+    rollDice();
+
+    score = rolls.reduce(totalScore);
+
+    function totalScore(total, num) {
+      return total + num;
+    }
+
+    console.log(score);
+
+    if (roll === 1) {
+      roll = 0;
+      score = 0;
+      $(".player1").toggleClass("activePlayer");
+      $(".player2").toggleClass("activePlayer");
+      // $(".currentRoll").toggle(".active");
+    }
+
+    $(".active").text(roll);
+    $(".currentScore").text(score);
+  });
+
+
+  $("form.rollTurn").submit(function(ev) {
+    ev.preventDefault();
+
+
+
+  });
+
+});
